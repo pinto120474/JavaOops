cca10b71dc63c2f685d9164c41d7cee3c39308e4
lpinto <pinto120474@gmail.com>
2020-07-30 17:42:49 +0530

    changes bird, fish and zoo functions

--- src/Bird.java
+++ src/Bird.java
@@ -1,6 +1,11 @@
 
-public class Bird {
+public class Bird extends Animal{
 	
+	public Bird(int age, String gender, int weightInLbs) {
+		super(age, gender, weightInLbs);
+		// TODO Auto-generated constructor stub
+	}
+
 	public void fly() {
 		System.out.println("flying....");
 	}
--- src/Fish.java
+++ src/Fish.java
@@ -1,5 +1,10 @@
 
-public class Fish {
+public class Fish extends Animal {
+
+	public Fish(int age, String gender, int weightInLbs) {
+		super(age, gender, weightInLbs);
+		// TODO Auto-generated constructor stub
+	}
 
 	public void swim() {
 		System.out.println("swimming.....");
